60d4452b51fa994b9ea6f5839b21d272cfac2b3f
Unknown <rafaelmonteiro95@gmail.com>
2018-08-10 17:17:38 -0300

    Fixed StepSoundController class name

--- Assets/scripts/StepSoundController.cs
+++ Assets/scripts/StepSoundController.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class stepSoundController : MonoBehaviour {
+public class StepSoundController : MonoBehaviour {
 
 	public AudioSource step1;
 	public AudioSource step2;
